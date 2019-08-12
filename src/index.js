3276b0ced08ecf1045f4c17bc0e53152874331ef
mankamolnar <markmanomolnar@gmail.com>
2019-08-12 22:27:41 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './components/app/components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
